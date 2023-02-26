4f2b435d0bea66dff5719abffb861b141aef5788
studyinglover <3046308220@qq.com>
2023-02-26 09:56:00 +0800

    update

--- scripts/inject.js
+++ scripts/inject.js
@@ -1,6 +1,6 @@
-// hexo.extend.filter.register('theme_inject', function(injects) {
-// 	injects.bodyEnd.raw('adsense', '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6818277566173475"crossorigin="anonymous"></script>');
-// 	injects.head.raw('adsense', '<style>ins.adsbygoogle[data-ad-status="unfilled"] { display: none !important; }</style>');
-// 	injects.postLeft.raw('adsense', '<aside class="sidebar d-none d-xl-block" style="margin-right:-1rem;z-index:-1"><ins class="adsbygoogle" style="display:flex;justify-content:center;min-width:160px;max-width:300px;width:100%;height:600px;position:sticky;top:2rem" data-ad-client="ca-pub-6818277566173475" data-ad-slot="4025700878"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({}); </script></aside>');
-// 	injects.postCopyright.raw('adsense', '<div style="width:100%;display:flex;justify-content:center;margin-bottom:1.5rem"><ins class="adsbygoogle" style="display:flex;justify-content:center;max-width:845px;width:100%;height:90px" data-ad-client="ca-pub-6818277566173475" data-ad-slot="4025700878"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({}); </script></div>');
-// });
+hexo.extend.filter.register('theme_inject', function(injects) {
+	injects.bodyEnd.raw('adsense', '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6818277566173475"crossorigin="anonymous"></script>');
+	injects.head.raw('adsense', '<style>ins.adsbygoogle[data-ad-status="unfilled"] { display: none !important; }</style>');
+	injects.postLeft.raw('adsense', '<aside class="sidebar d-none d-xl-block" style="margin-right:-1rem;z-index:-1"><ins class="adsbygoogle" style="display:flex;justify-content:center;min-width:160px;max-width:300px;width:100%;height:600px;position:sticky;top:2rem" data-ad-client="ca-pub-6818277566173475" data-ad-slot="4025700878"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({}); </script></aside>');
+	injects.postCopyright.raw('adsense', '<div style="width:100%;display:flex;justify-content:center;margin-bottom:1.5rem"><ins class="adsbygoogle" style="display:flex;justify-content:center;max-width:845px;width:100%;height:90px" data-ad-client="ca-pub-6818277566173475" data-ad-slot="4025700878"></ins><script> (adsbygoogle = window.adsbygoogle || []).push({}); </script></div>');
+});

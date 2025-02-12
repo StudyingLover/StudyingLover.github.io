aac09eb2da16a464d9a44388a40355f3a250e3bb
zjh <3046308220@qq.com>
2025-02-12 10:44:53 +0800

    fix atom.xml

--- src/pages/atom.xml.js
+++ src/pages/atom.xml.js
@@ -1,6 +1,7 @@
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { getAllPosts } from '@src/utils';
+import { getFluidPostPath } from '../utils';
 
 export async function GET(context) {
     const posts = await getAllPosts();
@@ -18,15 +19,20 @@ export async function GET(context) {
             <author>
                 <name>${SITE_TITLE}</name>
             </author>
-            ${posts.map((post) => `
+            ${posts.map((post) => {
+                const link = post.postType == "blog" 
+                    ? `${site}blog/${post.slug}/`
+                    : `${site}${getFluidPostPath(post).year}/${getFluidPostPath(post).month}/${getFluidPostPath(post).day}/${getFluidPostPath(post).title}`;
+                return `
                 <entry>
-                    <id>${site}/${post.slug}/</id>
+                    <id>${link}</id>
                     <title>${post.data.title}</title>
-                    <link href="${site}/${post.slug}/"/>
+                    <link href="${link}"/>
                     <updated>${new Date(post.data.pubDate).toISOString()}</updated>
                     <content type="html"><![CDATA[${post.data.description || ''}]]></content>
                 </entry>
-            `).join('')}
+                `;
+            }).join('')}
         </feed>`,
         {
             headers: {
